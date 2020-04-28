499f26d7bd3569d1d6711c3c911b24765e2622ef
andrenasx <andre2000.an@gmail.com>
2020-04-28 11:04:38 +0100

    Mutex to increment request number, and using pthread_detach

--- src/U.c
+++ src/U.c
@@ -3,21 +3,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/types.h>  
 #include <fcntl.h>
 #include <string.h>
 #include <pthread.h>
 #include "args.h"
 #include "utils.h"
 
-#define MAX_THREADS 100
-
-int i=0 ;
+int i=0;
+pthread_mutex_t client_mut=PTHREAD_MUTEX_INITIALIZER;
 
 void *thr_func(void *arg){
     int client_pid = getpid();
     long int client_tid = pthread_self();
     int duration = (rand() % (100 - 20 + 1)) + 20; //random duration in WC in ms
+
+    //Incrementing request nr
+    pthread_mutex_lock(&client_mut);
+    i++;
+    pthread_mutex_unlock(&client_mut);
 
     logRegister(i, client_pid, client_tid, duration, -1, "IWANT");
 
@@ -28,16 +32,15 @@ void *thr_func(void *arg){
     }
     printf("FIFO is open write\n");
 
-    //Send request to WC    
-    char message[256];
-
+    //Send request to WC
+    char message[MAX_LEN];
     sprintf(message, "[ %d, %d, %ld, %d, %d ]\n", i, client_pid, client_tid, duration, -1);
-    write(fd, &message, 256);
+    write(fd, &message, MAX_LEN);
     close(fd);
 
 
     //Reading WC response
-    char private_fifo[256];
+    char private_fifo[MAX_LEN];
     int id, pid, pl, dur, fd_private;
     long tid;
 
@@ -64,7 +67,7 @@ void *thr_func(void *arg){
         exit(1);
     }
 
-    if (read(fd_private, &message, 256)>0) printf(message);    //Reads message from private FIFO
+    if (read(fd_private, &message, MAX_LEN)>0) printf(message);    //Reads message from private FIFO
     sscanf(message, "[ %d, %d, %ld, %d, %d ]\n", &id, &pid, &tid, &dur, &pl);   
 
     if (pl == -1 && dur == -1){    //WC is closing
@@ -99,7 +102,8 @@ int main(int argc, char *argv[], char *envp[]){
 
     while(getElapsedTime()*1e-3<args.nsecs){
         pthread_create(&threads[id], NULL, thr_func, args.fifoname);
-        pthread_join(threads[id], NULL);
+        pthread_detach(threads[id]);
+        id++;
         sleep(1);
     }
 
