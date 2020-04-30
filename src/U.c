8601cf3180dd32cd323e559e1face641d4d191da
andrenasx <andre2000.an@gmail.com>
2020-04-30 14:16:15 +0100

    request struct

--- src/U.c
+++ src/U.c
@@ -5,37 +5,37 @@
 #include <sys/stat.h>
 #include <sys/types.h>  
 #include <fcntl.h>
-#include <string.h>
 #include <pthread.h>
 #include "args.h"
 #include "utils.h"
 
-int i=0;
-pthread_mutex_t client_mut=PTHREAD_MUTEX_INITIALIZER;
+struct request {
+    char* fifoname;
+    int i;
+};
+
+int closed = 0;
 
 void *thr_func(void *arg){
+    struct request r = *(struct request*) arg;
     int client_pid = getpid();
     long int client_tid = pthread_self();
     int duration = (rand() % (1000 - 200 + 1)) + 200; //random duration in WC in ms
 
     //Incrementing request nr
-    pthread_mutex_lock(&client_mut);
-    i++;
-    pthread_mutex_unlock(&client_mut);
 
-    logRegister(i, client_pid, client_tid, duration, -1, "IWANT");
-
-    int fd = open((char *)arg, O_WRONLY);
+    int fd = open(r.fifoname, O_WRONLY);
     if (fd==-1){
-        logRegister(i, client_pid, client_tid, duration, -1, "FAILD");
-        logRegister(i, client_pid, client_tid, duration, -1, "CLOSD");
+        closed = 1;
+        logRegister(r.i, client_pid, client_tid, duration, -1, "FAILD");
         return NULL;
     }
     printf("FIFO is open write\n");
+    logRegister(r.i, client_pid, client_tid, duration, -1, "IWANT");
 
     //Send request to WC
     char message[MAX_LEN];
-    sprintf(message, "[ %d, %d, %ld, %d, %d ]\n", i, client_pid, client_tid, duration, -1);
+    sprintf(message, "[ %d, %d, %ld, %d, %d ]\n", r.i, client_pid, client_tid, duration, -1);
     write(fd, &message, MAX_LEN);
     close(fd);
 
@@ -48,8 +48,9 @@ void *thr_func(void *arg){
     sprintf(private_fifo, "/tmp/%d.%ld", client_pid, client_tid);
     printf("Client pfifo %s\n", private_fifo);
     if (mkfifo(private_fifo, 0660) != 0){   //Makes private FIFO
+        logRegister(r.i, client_pid, client_tid, duration, -1, "FAILD");
         printf("Error, can't create private FIFO!\n");
-        exit(1);
+        return NULL;
     }
     else{
         printf("Private FIFO was created!\n");
@@ -59,7 +60,7 @@ void *thr_func(void *arg){
         printf("Private FIFO is open read %d\n", fd_private);
     }
     else{
-        logRegister(i, client_pid, client_tid, duration, -1, "FAILD");
+        logRegister(r.i, client_pid, client_tid, duration, -1, "FAILD");
         printf("Can't open FIFO\n");
         if(unlink(private_fifo) < 0)
             printf("Error can't destroy private FIFO!\n");
@@ -72,10 +73,10 @@ void *thr_func(void *arg){
     sscanf(message, "[ %d, %d, %ld, %d, %d ]\n", &id, &pid, &tid, &dur, &pl);   
 
     if (pl == -1 && dur == -1){    //WC is closing
-        logRegister(id, client_pid, client_tid, dur, pl, "CLOSD");
+        logRegister(r.i, client_pid, client_tid, dur, pl, "CLOSD");
     }
     else{    //WC is open
-        logRegister(id, client_pid, client_tid, dur, pl, "IAMIN");
+        logRegister(r.i, client_pid, client_tid, dur, pl, "IAMIN");
     }
 
     close(fd_private);    //Closes private FIFO
@@ -92,7 +93,7 @@ int main(int argc, char *argv[], char *envp[]){
     client_args args;
 
     if(get_client_args(&args, argc, argv)==-1){
-        perror("Error getting args!");
+        printf("Error getting args!\n");
         exit(1);
     }
 
@@ -101,8 +102,9 @@ int main(int argc, char *argv[], char *envp[]){
     pthread_t threads[MAX_THREADS];
     int id = 0;
 
-    while(getElapsedTime()*1e-3<args.nsecs){
-        pthread_create(&threads[id], NULL, thr_func, args.fifoname);
+    while(getElapsedTime()*1e-3<args.nsecs && !closed){
+        struct request r = {args.fifoname, id};
+        pthread_create(&threads[id], NULL, thr_func, &r);
         pthread_detach(threads[id]);
         usleep(500*1000); //create WC requests every 500 ms
         id++;
